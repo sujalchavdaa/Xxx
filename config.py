db96c1159852c4f311cf9e44e48b63742b7c898a
sujalchavdaa <sujalchavda500@gmail.com>
2025-06-05 15:44:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+api_id =26330942
+api_hash = "5de9fd033aa828dfd3bf0c28adeee660"
+bot_token = "7999358042:AAGXpAPxJkftNoUVKtzq4aQsmbs32wBEmf0"
+auth_users = "6883471516"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "56416564654"
+osowner_users = "6883471516"
 owner_users = [int(num) for num in osowner_users.split(",")]
