3ca9023403a7b76fe6805863340904176b23e646
sujalchavdaa <sujalchavda500@gmail.com>
2025-06-06 10:57:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id =546431313
-api_hash = ""
-bot_token = "aaf4235465a4r3a1f3af13af1a4fa"
-auth_users = "46513214"
+lapi_id =26330942
+api_hash = "5de9fd033aa828dfd3bf0c28adeee660"
+bot_token = "7763198696:AAE2eIfxmRZTnH--0MGW13gl_TsNXzriq9g"
+auth_users = "6883471516"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "56416564654"
 owner_users = [int(num) for num in osowner_users.split(",")]
